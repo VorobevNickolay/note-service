16f5c51383c9e7a6c5a2bba4118100614bcb6cf7
Николай <vorobevnickolai@gmail.com>
2022-09-19 20:06:45 +0300

    Add tests

---
+++ internal/pkg/user/models.go
@@ -0,0 +1,14 @@
+package user
+
+import "errors"
+
+type User struct {
+	ID       string
+	Username string
+	Password string
+}
+
+var (
+	ErrUserNotFound = errors.New("user was not found")
+	ErrUsedUsername = errors.New("username already in use")
+)
